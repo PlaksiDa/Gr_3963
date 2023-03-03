237331a0c6479d64a7dac346f89e4539624f3abb
Plaksin_Daniil <Ibiki5@mail.ru>
2023-03-03 15:56:13 +0300

    fix

--- Sem4Task27/Program.cs
+++ Sem4Task27/Program.cs
@@ -2,26 +2,47 @@
 // числе.
 // * Сделать оценку времени алгоритма через вещественные числа и строки
 
-int ReadData(string msg)
-{
-    Console.WriteLine(msg);
-    return int.Parse(Console.ReadLine() ?? "0");
-}
-
+//Метод для вывода
 void PrintResult(double line)
 {
     Console.WriteLine(line);
 }
-
-int SumCalc(int num)
+//Метод для расчёта суммы цифр в числе через остаток
+int SumCalcV1(int num)
 {
     int result = 0;
-    for (int i = 1; i <= num; i++)
+    while (num != 0)
     {
-        result = result + i;
+        result += num % 10;
+        num /= 10;
     }
     return result;
 }
-PrintResult(SumCalc(ReadData("Введите число: ")));
-// DateTime d1 = DateTime.Now;
-// Console.WriteLine(DateTime.Now - d1);
+//Метод для расчёта суммы цифр в числе через сумму элементов
+int SumCalcV2(string num)
+{
+    int result = 0;
+    for (int i = 0; i < num.Length; i++)
+    {
+        result += Convert.ToInt32(Convert.ToString(num[i]));
+    }
+    return result;
+}
+
+//Вводим число
+Console.WriteLine("Введите число:");
+string num = Console.ReadLine() ?? "0";
+
+//Создаем переменную с текущим временем
+DateTime d1 = DateTime.Now;
+//Конверуем строку в число, применяем его в первом методе
+PrintResult(SumCalcV1(Convert.ToInt32(num)));
+//Выводим разницу, между текущим временем и временем начала операции
+Console.WriteLine(DateTime.Now - d1);
+
+//Создаем переменную с текущим временем
+d1 = DateTime.Now;
+//Конверуем строку в число, применяем его в втором методе
+PrintResult(SumCalcV2(num));
+//Выводим разницу, между текущим временем и временем начала операции
+Console.WriteLine(DateTime.Now - d1);
--- Sem4Task29/Program.cs
+++ Sem4Task29/Program.cs
@@ -1,32 +1,63 @@
 ﻿// №29 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // * Ввести с клавиатуры длину массива и диапазон значений элементов 
 
+// ** Написать программу которая из имен через запятую выберет случайное
+// имя и выведет в терминал
+
+
 //Метод для вывода данных
-void Print1DArr(int[] arr)
-{
-    Console.Write("[");
-    for (int i = 0; i < arr.Length - 1; i++)
-    {
-        Console.Write(arr[i] + ", ");
-    }
-    Console.Write(arr[arr.Length - 1]);
-    Console.WriteLine("]");
-}
-//Метод для генерации элементов массива
-int[] Gen1DArr(int len, int min, int max)
-{
-    int[] arr = new int[len];
-    for (int i = 0; i < arr.Length; i++)
-    {
-        arr[i] = new Random().Next(min, max + 1);
-    }
-    return arr;
-}
-//Метод для ввода данных
-int ReadData(string msg)
+// void Print1DArr(int[] arr)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < arr.Length - 1; i++)
+//     {
+//         Console.Write(arr[i] + ", ");
+//     }
+//     Console.Write(arr[arr.Length - 1]);
+//     Console.WriteLine("]");
+// }
+
+// //Метод для генерации элементов массива
+// int[] Gen1DArr(int len, int min, int max)
+// {
+//     int[] arr = new int[len];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(min, max + 1);
+//     }
+//     return arr;
+// }
+
+// //Метод для ввода данных
+// int ReadData(string msg)
+// {
+//     Console.WriteLine(msg);
+//     return Convert.ToInt32(Console.ReadLine() ?? "0");
+// }
+
+// Print1DArr(Gen1DArr(ReadData("Длина массива: "), ReadData("Минимальная величина элемента массива"), ReadData("Максимальная величина элемента массива")));
+
+//2 Задача
+
+//Метод ввода данных
+string ReadData(string msg)
 {
     Console.WriteLine(msg);
-    return Convert.ToInt32(Console.ReadLine() ?? "0");
+    return Console.ReadLine() ?? "0";
+}
+//Метод вывода данных
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+//Метод выбора случайного имени
+string RndName(string names)
+{
+    //Строку с именами, методом Split, делим на элементы, указывая разделяющий символ и присваиваем их массиву
+    string[] arrNames = names.Split(",");
+    int rng = new Random().Next(0, arrNames.Length);
+    string name = arrNames[rng];
+    return name;
 }
 
-Print1DArr(Gen1DArr(ReadData("Длина массива: "), ReadData("Минимальная величина элемента массива"), ReadData("Максимальная величина элемента массива")));
+PrintResult(RndName(ReadData("Введите имена через запятую:")));
