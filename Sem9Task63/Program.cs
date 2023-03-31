c16057031aa83a52d1849eba47116e07534d0826
Plaksin_Daniil <Ibiki5@mail.ru>
2023-03-31 17:56:24 +0300

    add solution for task 63, 64, 65, 66, 67, 68, 69

---
+++ Sem9Task63/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 63.
+// Задайте значение N. Напиши программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии
+
+// Метод для ввода данных из консоли
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return Int32.Parse(Console.ReadLine() ?? "0");
+}
+//
+void LineGenetatorRec(int num)
+{
+    Console.Write(num + " ");
+    if (num == 1)
+    {
+    }
+    else
+    {
+        LineGenetatorRec(num - 1);
+    }
+}
+
+LineGenetatorRec(ReadData("Введите N:"));
---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: 
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Метод для ввода данных из консоли
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return Int32.Parse(Console.ReadLine() ?? "0");
+}
+// Метод для вывода
+void PrintResult(int line)
+{
+    Console.WriteLine(line);
+}
+// Метод для суммирования натуральных элементов в промежутке от M до N
+int RegSumDigMN(int m, int n)
+{
+    if (m >= n) return n;
+    else return m + RegSumDigMN(m + 1, n);
+}
+
+PrintResult(RegSumDigMN(ReadData("Введите m:"), ReadData("Введите n:")));
